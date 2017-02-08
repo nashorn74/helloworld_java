786d638c2574b39ce899388793c682894cf82a04
정금호 <nashorn74@gmail.com>
2017-02-08 15:09:33 +0900

    MemberInfoPrinter 클래스 추가

--- sp4-chap03/src/main/java/main/Main.java
+++ sp4-chap03/src/main/java/main/Main.java
@@ -1,16 +1,25 @@
 package main;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import assembler.Assembler;
 import spring.ChangePasswordService;
+import spring.MemberInfoPrinter;
 import spring.MemberRegisterService;
 import spring.RegisterRequest;
 
 public class Main {
-	private static Assembler assembler = new Assembler();
+	//private static Assembler assembler = new Assembler();
+	private static ApplicationContext ctx = null;
 	public static void main(String[] args) {
+		ctx = new GenericXmlApplicationContext(
+				"classpath:applicationContext.xml");
 		////////////////////////////////////////////////////////회원가입
 		MemberRegisterService registerService =
-				assembler.getMemberRegisterService();
+				//assembler.getMemberRegisterService();
+				ctx.getBean("memberRegisterService",
+						MemberRegisterService.class);
 		RegisterRequest request = new RegisterRequest();
 		request.setEmail("test@test.net");
 		request.setName("홍길동");
@@ -22,13 +31,19 @@ public class Main {
 			e.printStackTrace();
 		}
 		ChangePasswordService changePasswordService = 
-				assembler.getChangePasswordService();//암호변경
+				//assembler.getChangePasswordService();//암호변경
+				ctx.getBean("changePasswordService",
+						ChangePasswordService.class);
 		try {
 			changePasswordService.changePassword("test@test.net", 
 					"1234", "7777");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		//////////////////////////////////
+		MemberInfoPrinter infoPrinter = 
+				ctx.getBean("infoPrinter", MemberInfoPrinter.class);
+		infoPrinter.printMemberInfo("test@test.net");
 
 	}
 
