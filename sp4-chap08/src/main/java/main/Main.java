08174241e203a0f8cdbe2f16a0e4f16454da00bb
정금호 <nashorn74@gmail.com>
2017-02-13 11:19:31 +0900

    insert, update, count, select all 기능 추가

--- sp4-chap08/src/main/java/main/Main.java
+++ sp4-chap08/src/main/java/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
@@ -16,6 +19,32 @@ public class Main {
 		
 		Member member = memberDao.selectByEmail("test@test.net");
 		System.out.println(member.getName());
+		
+		System.out.println("--------------------------------------");
+		List<Member> memberList = memberDao.selectAll();
+		for (int i = 0; i < memberList.size(); i++) {
+			Member mem = memberList.get(i);
+			System.out.println((i+1)+":"+mem.getName());
+		}
+		
+		System.out.println("count="+memberDao.count());
+		
+		Member updateMember = 
+				new Member("test@test.net","9999","홍길동",null);
+		memberDao.update(updateMember);
+		member = memberDao.selectByEmail("test@test.net");
+		System.out.println(member.getName());
+		
+		Member insertMember =
+			new Member("hello@world.com","1111","헬로월드",new Date());
+		memberDao.insert(insertMember);
+		System.out.println(insertMember.getId());
+		System.out.println("--------------------------------------");
+		memberList = memberDao.selectAll();
+		for (int i = 0; i < memberList.size(); i++) {
+			Member mem = memberList.get(i);
+			System.out.println((i+1)+":"+mem.getName());
+		}
 		
 		ctx.close();
 	}
--- sp4-chap08/src/main/java/spring/MemberDao.java
+++ sp4-chap08/src/main/java/spring/MemberDao.java
@@ -1,7 +1,10 @@
 package spring;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -10,7 +13,10 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 
 public class MemberDao {
 	
@@ -36,13 +42,51 @@ public class MemberDao {
 				email);
 		return results.isEmpty()?null:results.get(0);
 	}
-	public void insert(Member member) {
-		
+	public int count() {
+		Integer count = jdbcTemplate.queryForObject(
+				"select count(*) from MEMBER", Integer.class);
+		return count;
+	}
+	public void insert(final Member member) {
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				PreparedStatement pstmt = con.prepareStatement(
+					"insert into MEMBER(EMAIL,PASSWORD,NAME,REGDATE) "+
+					"values(?,?,?,?)", new String[] {"ID"});
+				pstmt.setString(1,member.getEmail());
+				pstmt.setString(2, member.getPassword());
+				pstmt.setString(3, member.getName());
+				pstmt.setTimestamp(4, new Timestamp(
+						member.getRegisterDate().getTime()));
+				return pstmt;
+			}
+		}, keyHolder);
+		Number keyValue = keyHolder.getKey();
+		member.setId(keyValue.longValue());
 	}
 	public void update(Member member) {
-		
+		jdbcTemplate.update(
+				"update MEMBER set NAME=?,PASSWORD=? where EMAIL=?",
+				member.getName(), member.getPassword(),
+				member.getEmail());
 	}
-	public Collection<Member> selectAll() {
-		return null;
+	public List<Member> selectAll() {
+		List<Member> results = jdbcTemplate.query(
+			"select * from MEMBER", 
+			new RowMapper<Member>() {
+				@Override
+				public Member mapRow(ResultSet rs, int rowNum) throws SQLException {
+					Member member = new Member(
+							rs.getString("EMAIL"),
+							rs.getString("PASSWORD"),
+							rs.getString("NAME"),
+							rs.getTimestamp("REGDATE"));
+					member.setId(rs.getLong("ID"));
+					return member;
+				}
+			});
+		return results;
 	}
 }
